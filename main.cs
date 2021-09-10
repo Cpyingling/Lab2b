937a005202e586bcae18a6150cb2f81fafd63206
Charlie Y <89798338+Cpyingling@users.noreply.github.com>
2021-09-10 02:54:34 +0000

    fixed pointless use of double(I believe its pointless?) changed to int for performance

--- main.cs
+++ main.cs
@@ -10,23 +10,23 @@ class Program {
     Console.Write ("Enter another number: ");
     int num2 = Convert.ToInt32(Console.ReadLine());
     //Add numbers and save to sum
-    double sum = num1 + num2;
+    int sum = num1 + num2;
     //display sum
     Console.WriteLine ("Sum = " +sum);
     //Subtract number 2 from 1 and save as diff
-    double diff = num1 - num2;
+    int diff = num1 - num2;
     //display diff
     Console.WriteLine ("Diff = " +diff);
     //* 2 numbers and save as product
-    double product = num1 * num2;
+    int product = num1 * num2;
     //display product
     Console.WriteLine ("Product = " +product);
     //divide num 1 by 2 and save as quotient
-    double quotient = Convert.ToInt32(num1 / num2); //displays 2
+    int quotient = num1 / num2; //displays 2
     //display quotient
     Console.WriteLine ("Quotient = " +quotient);
     //find remainder using % on num 1 & 2 and save as remainder
-    double remainder = num1 % num2;
+    int remainder = num1 % num2;
     //display remainder
     Console.WriteLine ("Remainder = " +remainder);
   }
